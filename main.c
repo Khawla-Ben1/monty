4fc006fab653571890c6eabe5d2ae1ef89122089
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-08-10 22:25:43 +0100

    push

--- main.c
+++ main.c
@@ -5,48 +5,48 @@
 * @argc: argument count
 * @argv: argument value
 *
-* Return
+* Return : exit success
 */
 
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    char line[256];
-    stack_t *stack = NULL;
-    unsigned int line_number = 1;
+	FILE *file;
+	char line[256];
+	stack_t *stack = NULL;
+	unsigned int line_number = 1;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-    while (fgets(line, sizeof(line), file))
-    {
-        char *opcode;
-        char *line_copy = strdup(line);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (fgets(line, sizeof(line), file))
+	{
+		char *opcode;
+		char *line_copy = strdup(line);
 
-        if (line_copy == NULL)
-        {
-            fprintf(stderr, "Error: malloc failed\n");
-            exit(EXIT_FAILURE);
-        }
+		if (line_copy == NULL)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			exit(EXIT_FAILURE);
+		}
 
-        opcode = strtok(line_copy, " \t\n");
-        if (opcode && opcode[0] != '#')
-        {
-            execute_opcode(opcode, &stack, line_number);
-        }
+		opcode = strtok(line_copy, " \t\n");
+		if (opcode && opcode[0] != '#')
+		{
+			execute_opcode(opcode, &stack, line_number);
+		}
 
-        free(line_copy);
-        line_number++;
-    }
-    fclose(file);
-    free_stack(stack);
-    return (EXIT_SUCCESS);
+		free(line_copy);
+		line_number++;
+	}
+	fclose(file);
+	free_stack(stack);
+	return (EXIT_SUCCESS);
 }
--- monty.c
+++ monty.c
@@ -1,83 +1,112 @@
 #include "monty.h"
 
 
-/* Helper function to free the stack */
+/**
+ * free_stack - Frees the memory allocated for the stack.
+ * @stack: Pointer to the head of the stack.
+ *
+ * Description: This function iterates through the stack, freeing each node.
+ */
 void free_stack(stack_t *stack)
 {
-    stack_t *tmp;
+	stack_t *tmp;
 
-    while (stack)
-    {
-        tmp = stack;
-        stack = stack->next;
-        free(tmp);
-    }
+	while (stack)
+	{
+		tmp = stack;
+		stack = stack->next;
+		free(tmp);
+	}
 }
 
-/* Function to handle the push opcode */
+/**
+ * push - Handles the push opcode.
+ * @stack: Pointer to the head of the stack.
+ * @line_number: The current line number in the bytecode file.
+ *
+ * Description: This function pushes an integer onto the stack.
+ * It parses the argument after the "push" opcode and adds it to the stack.
+ * If the argument is invalid, it prints an error message and exits.
+ */
 void push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *new_node;
-    char *arg;
-    int n;
+	stack_t *new_node;
+	char *arg;
+	int n;
 
-    arg = strtok(NULL, " \t\n");
-    if (arg == NULL || !isdigit(arg[0]))
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	arg = strtok(NULL, " \t\n");
+	if (arg == NULL || !isdigit(arg[0]) && (arg[0] != '-' || !isdigit(arg[1])))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    n = atoi(arg);
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	n = atoi(arg);
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = n;
-    new_node->next = *stack;
-    new_node->prev = NULL;
-    if (*stack)
-        (*stack)->prev = new_node;
-    *stack = new_node;
+	new_node->n = n;
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	if (*stack)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
 
-/* Function to handle the pall opcode */
+/**
+ * pall - Handles the pall opcode.
+ * @stack: Pointer to the head of the stack.
+ * @line_number: The current line number in the bytecode file.
+ *
+ * Description: This function prints all the values on the stack, starting
+ * from the top of the stack. If the stack is empty, it does not print anything.
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
+	stack_t *current = *stack;
 
-    (void)line_number; /* Unused parameter */
+	(void)line_number; /* Unused parameter */
 
-    while (current)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
 
-/* Function to execute an opcode */
+/**
+ * execute_opcode - Executes the given opcode.
+ * @opcode: The opcode to be executed.
+ * @stack: Pointer to the head of the stack.
+ * @line_number: The current line number in the bytecode file.
+ *
+ * Description: This function matches the opcode with the corresponding
+ * function and executes it. If the opcode is not recognized, it prints
+ * an error message and exits.
+ */
 void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number)
 {
-    instruction_t instructions[] = {
-        {"push", push},
-        {"pall", pall},
-        {NULL, NULL}
-    };
-    int i;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+	int i;
 
-    for (i = 0; instructions[i].opcode; i++)
-    {
-        if (strcmp(opcode, instructions[i].opcode) == 0)
-        {
-            instructions[i].f(stack, line_number);
-            return;
-        }
-    }
+	for (i = 0; instructions[i].opcode; i++)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+			instructions[i].f(stack, line_number);
+			return;
+		}
+	}
 
-    fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-    exit(EXIT_FAILURE);
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
 }
 
